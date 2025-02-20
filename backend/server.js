755c6275d76a9121ddad3f400bb4c42e6181103a
Avaneeshakrishna <avaneesh.shastry@gmail.com>
2025-02-20 12:13:18 -0500

    3- payload limit is updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 const app = express();
 const  PORT = process.env.PORT || 5000;
 
-app.use(express.json()); //to parse request.body
+app.use(express.json({limit:"6mb"})); //to parse request.body and limit shouldn't be too high to prevent DDOS attack
 app.use(express.urlencoded({extended: true}));  //to parse form data(urlencoded)
 // console.log(process.env.MONGO_URI);
 
